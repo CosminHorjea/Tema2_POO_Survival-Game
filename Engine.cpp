990c65616d95738826397b02cfc5ac31312f556c
CosminHorjea <horjeacosmin@gmail.com>
2020-04-29 00:47:44 +0300

    game loop

--- Engine.cpp
+++ Engine.cpp
@@ -5,9 +5,22 @@ using namespace std;
 
 Game::Game()
 {
-	int n;
+	int n, done = 0;
+	char response;
 	cout << "Introduceti marimea hartii: ";
 	cin >> n;
 	map = new Map(n, n);
-	cout << *map;
+	//TODO:
+	//		-> map.place items()
+	//      -> map.place Agents()
+	while (!done) //game loop
+	{
+		cout << *map;
+		cout << "Doriti sa rulati o noua runda (Y)es/(N)o: ";
+		//TODO map.muta_agenti();
+		//TODO maybe mapa.drop_items();
+		cin >> response;
+		if (response == 'N')
+			done = 1;
+	}
 }
