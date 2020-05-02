c3f7230b9e69ca00ea100b6351c2b1b774592830
CosminHorjea <horjeacosmin@gmail.com>
2020-05-02 22:28:16 +0300

    idk

--- AgentAsasin.h
+++ AgentAsasin.h
@@ -12,10 +12,7 @@ class Asasin : public Agent
 public:
 	Asasin(const string name, pair<int, int> position) : Agent(name, position, 50, 100, 3, 2)
 	{
-		std::cout << "Moves";
-		// cout << moves[1].first;
-		Agent::possibleMoves = moves;
-		cout << Agent::possibleMoves[1].first;
+		this->Agent::possibleMoves = moves;
 	}
 	string getAgentType() { return "Asasin"; }
 	pair<int, int> chooseNextPosition(Map &) const;
